dffd083c974ce40251903398ac7e2306266c913c
Mezz <mezzanine510@gmail.com>
2023-11-22 11:24:36 -0800

    Added BasePawn, Tank classes; Added BP for Tank and Turret

---
+++ Source/ToonTanks/BasePawn.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "BasePawn.generated.h"
+
+UCLASS()
+class TOONTANKS_API ABasePawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ABasePawn();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	int32 VisibleAnywhereInt = 12;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 EditAnywhereInt = 22;
+
+	UPROPERTY(VisibleInstanceOnly)
+	int32 VisibleInstanceOnly = 11;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	float VisibleDefaultOnly = 2;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	class UCapsuleComponent* CapsuleComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* BaseMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* TurretMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	USceneComponent* ProjectileSpawnPoint;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Super Duper Variables",meta = (AllowPrivateAccess = "true"))
+	int32 EditDefaultOnlyInt = 9;
+
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Super Duper Variables", meta = (AllowPrivateAccess = "true"))
+	int32 EditInstanceOnlyInt = 14;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
---
+++ Source/ToonTanks/Tank.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Tank.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class TOONTANKS_API ATank : public ABasePawn
+{
+	GENERATED_BODY()
+
+public:
+
+	ATank();
+
+private:
+
+    UPROPERTY(VisibleAnywhere, Category = "Camera")
+	class USpringArmComponent* SpringArm;
+
+    UPROPERTY(VisibleAnywhere, Category = "Camera")
+	class UCameraComponent* Camera;
+};
