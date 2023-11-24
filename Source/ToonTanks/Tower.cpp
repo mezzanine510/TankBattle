c2a8beef5c0be5e3c50639532811f54ea558614a
Mezz <mezzanine510@gmail.com>
2023-11-24 15:26:27 -0800

    created Projectile class and BP

--- Source/ToonTanks/Tower.cpp
+++ Source/ToonTanks/Tower.cpp
@@ -4,20 +4,13 @@
 #include "Tower.h"
 #include "Tank.h"
 #include "Kismet/GameplayStatics.h"
+// #include ""
 
 void ATower::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
 
-    if (Tank)
-    {
-        FVector TankLocation = Tank->GetActorLocation();
-        float Distance = FVector::Dist(GetActorLocation(), TankLocation);
-
-        if (Distance > FireRange) return;
-
-        RotateTurret(TankLocation);
-    }
+    if (InFireRange()) RotateTurret(Tank->GetActorLocation());
 }
 
 void ATower::BeginPlay()
@@ -25,4 +18,23 @@ void ATower::BeginPlay()
     Super::BeginPlay();
 
     Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+
+    GetWorldTimerManager().SetTimer(FireRateTimerHandle, this, &ATower::CheckFireCondition, FireRate, true);
+}
+
+void ATower::CheckFireCondition()
+{
+    // FireRateTimerHandle
+    if (InFireRange()) Fire();
+}
+
+bool ATower::InFireRange()
+{
+    if (!Tank) return false;
+
+    FVector TankLocation = Tank->GetActorLocation();
+    float Distance = FVector::Dist(GetActorLocation(), TankLocation);
+
+    if (Distance > FireRange) return false;
+    else return true;
 }
